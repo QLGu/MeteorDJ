492c7cfabd822528ff287f17ae0b6a3d4e307765
Lasercar7 <lasercar@users.noreply.github.com>
2015-11-15 20:28:01 -0600

    playlist collection

--- MeteorDJ.js
+++ MeteorDJ.js
@@ -1,6 +1,15 @@
+Playlist = new Mongo.Collection("playlist");
+//TODO replace with user-specefic playlists
+
 if (Meteor.isClient) {
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
+  });
+
+  Template.body.helpers({
+    playlist: function() {
+      return Playlist.find();
+    }
   });
 }
 
