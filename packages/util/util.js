7d9562137532773b264c5e1327896d10ed80593c
Lasercar7 <lasercar@users.noreply.github.com>
2015-11-17 00:58:51 -0600

    small scoping tweak

--- packages/util/util.js
+++ packages/util/util.js
@@ -3,12 +3,12 @@ Util = {
     //TODO test very basically if is youtube or soundcloud domain at all first?
     //TODO to save processing time
 
-    var extracted = false;
+    var extracted;
 
     var patternYoutube = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
     var matchYoutube = url.match(patternYoutube);
     if (matchYoutube) {
-      var extracted = {
+      extracted = {
         code: matchYoutube[1],
         platform: "youtube"
       };
@@ -18,7 +18,7 @@ Util = {
     var patternSoundcloud = /(^https?:\/\/soundcloud\.com\/.*)$/;
     var matchSoundcloud = url.match(patternSoundcloud);
     if (matchSoundcloud) {
-      var extracted = {
+      extracted = {
         code: matchSoundcloud[1],
         platform: "soundcloud"
       };
